d33a1c97a313f997f1bb434af188a0ee20d1272c
Paul J. Lucas <paul@lucasmail.org>
2000-03-19 18:20:40 +0000

    Added search daemon configuration paramaters.

--- swish++/trunk/config.h
+++ swish++/trunk/config.h
@@ -26,8 +26,8 @@
 
 int const	Word_Hard_Min_Size		= 3;
 int const	Word_Hard_Max_Size		= 25;
-//		The minimum and maximum lengths a word must be in order even
-//		to bother doing more aggressive checks on to determine if it
+//		The minimum and maximum lengths a word must be in order even to
+//		bother doing more aggressive checks on to determine if it
 //		should be indexed.
 
 int const	Word_Min_Size			= 4;
@@ -41,9 +41,9 @@ int const	Word_Min_Vowels			= 1;
 int const	Word_Hex_Max_Size		= 4;
 //		The maximum length a string composed entirely of hexadecimal
 //		digits i.e., ASCII hex data, can be before it is discarded.
-//		Note that the word "cafe" is a legitimate English word
-//		composed entirely of hexedecimal digits.  This parameter is
-//		used only by extract(1) in extract.c.
+//		Note that the word "cafe" is a legitimate English word composed
+//		entirely of hexedecimal digits.  This parameter is used only by
+//		extract(1) in extract.c.
 
 // I don't think there is a word in English that has more than...
 
@@ -64,18 +64,17 @@ int const	Word_Max_Consec_Puncts		= 1;
 //
 char const	Word_Chars[] = "&'-0123456789abcdefghijklmnopqrstuvwxyz_";
 //		Characters that may be in a word.  Note that '&' is here so
-//		acronyms like "AT&T" are treated as one word.  Unlike
-//		SWISH-E, ';' does not need to be here to recognize and
-//		convert character entity references.
+//		acronyms like "AT&T" are treated as one word.  Unlike SWISH-E,
+//		';' does not need to be here to recognize and convert character
+//		entity references.
 
 #define		OPTIMIZE_WORD_CHARS		1
 //		If you are using the default set of characters, that is the
-//		alphanumerics and "&'-_" characters, then having this macro
-//		set to 1 will optimize the is_word_char() function yielding
-//		about a 10% performance improvement; alternatively, you can
-//		also edit that function to keep the optimization if you are
-//		not using the default set of characters.  See word_util.h for
-//		details.
+//		alphanumerics and "&'-_" characters, then having this macro set
+//		to 1 will optimize the is_word_char() function yielding about a
+//		10% performance improvement; alternatively, you can also edit
+//		that function to keep the optimization if you are not using the
+//		default set of characters.  See word_util.h for details.
 
 char const	Word_Begin_Chars[] = "0123456789abcdefghijklmnopqrstuvwxyz";
 //		Characters that may begin a word; should be a subset of the
@@ -85,15 +84,14 @@ char const	Word_Begin_Chars[] = "0123456789abcdefghijklmnopqrstuvwxyz";
 //		Same deal as with OPTIMIZE_WORD_CHARS.
 
 char const	Word_End_Chars[] = "0123456789abcdefghijklmnopqrstuvwxyz";
-//		Characters that may end a word; usually the same as the
-//		above.
+//		Characters that may end a word; usually the same as the above.
 
 #define		OPTIMIZE_WORD_END_CHARS		1
 //		Same deal as with OPTIMIZE_WORD_CHARS.
 
 ////////// HTML parameters ////////////////////////////////////////////////////
 
-int const	Entity_Max_Size			= 7;
+int const	Entity_Max_Size			= 6;
 //		The maximum size of an entity reference, e.g., "&eacute;" NOT
 //		counting the leading '&' or the trailing ';'.  You should have
 //		no reason to change this.
@@ -112,11 +110,54 @@ int const	TitleLines_Default		= 12;
 int const	Title_Max_Size			= 200;
 //		Maximum length of a title between <TITLE>...</TITLE>.
 
+#ifdef	SEARCH_DAEMON
+////////// Search server daemon parameters ////////////////////////////////////
+
+char const	SocketFile_Default[]		= "/tmp/swish++.socket";
+//		Default name of the Unix domain socket file; this can be
+//		overridden either in a config. file or on the command line.
+
+int const	SocketQueueSize_Default		= 100;
+//		Maximum number of queued connections for a socket.  From
+//		[Stevens 1998], p. 96:
+//
+//			Historically, sample code always shows a backlog of 5,
+//			as that was the maximum value supported by 4.2BSD.
+//			This was adequate in the 1980s when busy servers would
+//			handle only a few hundred connections per day.  But
+//			with the growth of the World Wide Web (WWW), where busy
+//			servers handle millions of connections per day, this
+//			small number is completely inadequate.  Busy HTTP
+//			servers must specify a much larger backlog, and newer
+//			kernels must support larger values.
+//
+//		This can be overridden either in a config. file or on the
+//		command line.
+
+int const	SocketTimeout_Default		= 10; // seconds
+//		The number of seconds a client has to complete a search request
+//		before being disconnected.  This is to prevent a client from
+//		connecting, not completing a request, and causing the thread
+//		servicing the request to wait forever.
+
+int const	ThreadsMin_Default		= 5;
+//		The minimum number of simultanous threads; this can be
+//		overridden either in a config. file or on the command line.
+
+int const	ThreadsMax_Default		= 100;
+//		The maximum number of simultanous threads; this can be
+//		overridden either in a config. file or on the command line.
+
+int const	ThreadTimeout_Default		= 30; // seconds
+//		The number of seconds until an idle spare thread times out and
+//		destroys itself.
+#endif
+
 ////////// Miscellaneous parameters ///////////////////////////////////////////
 
 char const	ConfigFile_Default[]		= "swish++.conf";
-//		Default name of the configuration file; this can be
-//		overridden on the command line.
+//		Default name of the configuration file; this can be overridden
+//		on the command line.
 
 int const	FilesReserve_Default		= 1000;
 //		Default maximum number of files to reserve space for; see
@@ -145,9 +186,9 @@ char const	TempDirectory_Default[]		= "/tmp";
 //		and more files get created in /tmp, you will have less swap
 //		space, indexing will get slower, and you may run out of memory.
 //		If this is the case, you can either change this default here
-//		for all users or override it either in a config. file or on
-//		the command line to use a directory on a real filesystem,
-//		i.e., one on a physical disk.  The directory must exist.
+//		for all users or override it either in a config. file or on the
+//		command line to use a directory on a real filesystem, i.e., one
+//		on a physical disk.  The directory must exist.
 
 int const	WordPercentMax_Default		= 100;
 //		Default maximum percentage of files a word may occur in before
@@ -156,10 +197,13 @@ int const	WordPercentMax_Default		= 100;
 
 int const	Word_Threshold			= 250000;
 //		The word count past which partial indicies are generated and
-//		merged since the words are too big to fit into memory.  If
-//		you index and your machine begins to swap like mad, lower
-//		this value.  The above works OK in a 64MB machine.  A rule of
-//		thumb is to add 250000 words for each additional 64MB of RAM
-//		you have.
+//		merged since all the words are too big to fit into memory at
+//		the same time.  If you index and your machine begins to swap
+//		like mad, lower this value.  The above works OK in a 64MB
+//		machine.  A rule of thumb is to add 250000 words for each
+//		additional 64MB of RAM you have.  These numbers are for a SPARC
+//		machine running Solaris.  Other machines running other
+//		operating systems use memory differently.  You simply have to
+//		experiment.
 
 #endif	/* config_H */
