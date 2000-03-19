8c5065aa01512b89b4911e0612821d1ea84a0b4d
Paul J. Lucas <paul@lucasmail.org>
2000-03-19 05:16:22 +0000

    Added comments; added verbosity code.

--- swish++/trunk/directory.c
+++ swish++/trunk/directory.c
@@ -23,8 +23,6 @@
 #include <iostream>
 #include <queue>
 #include <string>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <dirent.h>
 
 // local
@@ -41,10 +39,6 @@ extern RecurseSubdirs	recurse_subdirectories;
 extern Verbosity	verbosity;
 
 extern void	do_file( char const *file_name );
-		// Note that there are two do_file() functions: one each in
-		// extract.c and index.c.  The do_directory() function below
-		// calls whichever one it's linked into an executable with,
-		// either "extract" or "index" respectively.
 
 #ifndef	PJL_NO_SYMBOLIC_LINKS
 FollowLinks	follow_symbolic_links;
@@ -78,18 +72,31 @@ FollowLinks	follow_symbolic_links;
 	static dir_queue_type dir_queue;
 	static int recursion;
 
+	if ( verbosity > 1 ) {
+		if ( verbosity > 2 ) cout << '\n';
+		cout << dir_name << flush;
+	}
+
 #ifndef	PJL_NO_SYMBOLIC_LINKS
-	if ( is_symbolic_link( dir_name ) && !follow_symbolic_links )
+	if ( is_symbolic_link( dir_name ) && !follow_symbolic_links ) {
+		if ( verbosity > 3 )
+			cout << " (skipped: symbolic link)";
+		if ( verbosity > 1 )
+			cout << '\n';
 		return;
+	}
 #endif
 
 	DIR *const dir_p = ::opendir( dir_name );
-	if ( !dir_p )					// can't open: skip
+	if ( !dir_p ) {
+		if ( verbosity > 3 )
+			cout << " (skipped: can not open)";
+		if ( verbosity > 1 )
+			cout << '\n';
 		return;
+	}
 
 	if ( verbosity > 1 ) {
-		if ( verbosity > 2 ) cout << '\n';
-		cout << dir_name;
 		if ( verbosity > 2 ) cout << ':';
 		cout << '\n';
 	}
@@ -103,8 +110,16 @@ FollowLinks	follow_symbolic_links;
 		string const path( ( dir_string + '/' ) + dir_ent->d_name );
 		if ( is_directory( path ) && recurse_subdirectories )
 			dir_queue.push( path );
-		else
+		else {
+			// Note that do_file() is called in the case where
+			// 'path' is a directory and recurse_subdirectories is
+			// false.  This is OK since do_file() checks for and
+			// only does plain files.  It's also desirable to call
+			// do_file() so we don't have to repeat the code to
+			// print verbose information for 'path'.
+			//
 			do_file( path.c_str() );
+		}
 	}
 
 	::closedir( dir_p );
@@ -114,7 +129,7 @@ FollowLinks	follow_symbolic_links;
 	////////// Do all subdirectories //////////////////////////////////////
 
 	while ( !dir_queue.empty() ) {
-		dir_queue_type::value_type dir_name = dir_queue.front();
+		dir_queue_type::value_type const dir_name = dir_queue.front();
 		dir_queue.pop();
 		++recursion;
 		do_directory( dir_name.c_str() );
