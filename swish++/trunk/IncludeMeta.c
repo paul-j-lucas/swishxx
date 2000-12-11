fa1f5541748e30e67c46ec6089d4e855e8ea3e6e
Paul J. Lucas <paul@lucasmail.org>
2000-12-11 07:11:17 +0000

    Initial version in CVS.

---
+++ swish++/trunk/IncludeMeta.c
@@ -0,0 +1,71 @@
+/*
+**	SWISH++
+**	IncludeMeta.c
+**
+**	Copyright (C) 2000  Paul J. Lucas
+**
+**	This program is free software; you can redistribute it and/or modify
+**	it under the terms of the GNU General Public License as published by
+**	the Free Software Foundation; either version 2 of the License, or
+**	(at your option) any later version.
+** 
+**	This program is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**	GNU General Public License for more details.
+** 
+**	You should have received a copy of the GNU General Public License
+**	along with this program; if not, write to the Free Software
+**	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+// standard
+#include <cstring>
+
+// local
+#include "IncludeMeta.h"
+#include "exit_codes.h"
+#include "util.h"
+
+//*****************************************************************************
+//
+// SYNOPSIS
+//
+	/* virtual */ void IncludeMeta::parse_value( char *line )
+//
+// DESCRIPTION
+//
+//	Parse the line of text by splitting it into words that are separated by
+//	whitespace.  Additionally, "words" can be further split by '=' to do
+//	meta name reassignment, e.g.:
+//
+//		adr=address
+//
+// PARAMETERS
+//
+//	line	The line of text to be parsed.
+//
+//*****************************************************************************
+{
+	char *lower = to_lower( line );
+	for (	register char const *meta_name;
+		meta_name = ::strtok( lower, " \r\t" );
+		lower = 0
+	) {
+		//
+		// See if the meta name contains a reassignment: if so, chop it
+		// at the '='.
+		//
+		char *reassign = ::strchr( meta_name, '=' );
+		if ( reassign ) {
+			*reassign = '\0';
+			if ( !*++reassign ) {
+				cerr	<< error << "name expected after '='"
+					<< endl;
+				::exit( Exit_Config_File );
+			}
+		}
+		char const *const m = ::strdup( meta_name );
+		insert( value_type( m, reassign ? ::strdup( reassign ) : m ) );
+	}
+}
