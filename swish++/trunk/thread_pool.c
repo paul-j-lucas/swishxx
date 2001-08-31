5a354904e6bed653d403b7d52c273413aeef503d
Paul J. Lucas <paul@lucasmail.org>
2001-08-31 23:33:21 +0000

    Took while ( !::pthread_kill( thread_, 0 ) ) ; back out.

--- swish++/trunk/thread_pool.c
+++ swish++/trunk/thread_pool.c
@@ -335,10 +335,6 @@ namespace PJL {
 	if ( !destructing_ ) {
 		destructing_ = true;
 		::pthread_cancel( thread_ );
-		//
-		// Wait until the thread is really dead.
-		//
-		while ( !::pthread_kill( thread_, 0 ) ) ;
 	}
 }
 
