bf3315410f31fb6293578df4e11e3ace5f9adc4d
Paul J. Lucas <paul@lucasmail.org>
2001-10-09 15:57:09 +0000

    This commit was manufactured by cvs2svn to create tag 'swish++-5.4.2'.

---
+++ swish++/tags/swish++-5.4.2/classic_formatter.h
@@ -0,0 +1,49 @@
+/*
+**	SWISH++
+**	classic_formatter.h
+**
+**	Copyright (C) 1998  Paul J. Lucas
+**
+**	This program is free software; you can redistribute it and/or modify
+**	it under the terms of the GNU General Public License as published by
+**	the Free Software Foundation; either version 2 of the License, or
+**	(at your option) any later version.
+**
+**	This program is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**	GNU General Public License for more details.
+**
+**	You should have received a copy of the GNU General Public License
+**	along with this program; if not, write to the Free Software
+**	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef	classic_formatter_H
+#define	classic_formatter_H
+
+// local
+#include "results_formatter.h"
+
+//*****************************************************************************
+//
+// SYNOPSIS
+//
+	class classic_formatter : public results_formatter
+//
+// DESCRIPTION
+//
+//	A classic_formatter is-a results_formatter for formatting search
+//	results in the "classic" SWISH++ format.
+//
+//*****************************************************************************
+{
+public:
+	classic_formatter( std::ostream &o, int results ) :
+		results_formatter( o, results ) { }
+
+	virtual void pre( stop_word_set const& ) const;
+	virtual void result( int rank, file_info const& ) const;
+};
+
+#endif	/* classic_formatter_H */
