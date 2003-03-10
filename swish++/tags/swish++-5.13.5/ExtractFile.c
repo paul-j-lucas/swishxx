dd322b73b09922d8cc81ab1ee5dbb7444dfd005c
Paul J. Lucas <paul@lucasmail.org>
2003-03-10 19:38:48 +0000

    This commit was manufactured by cvs2svn to create tag 'swish++-5.13.5'.

---
+++ swish++/tags/swish++-5.13.5/ExtractFile.c
@@ -0,0 +1,53 @@
+/*
+**	SWISH++
+**	ExtractFile.c
+**
+**	Copyright (C) 2000  Paul J. Lucas
+**
+**	This program is free software; you can redistribute it and/or modify
+**	it under the terms of the GNU General Public License as published by
+**	the Free Software Foundation; either version 2 of the License, or
+**	(at your option) any later version.
+**
+**	This program is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**	GNU General Public License for more details.
+**
+**	You should have received a copy of the GNU General Public License
+**	along with this program; if not, write to the Free Software
+**	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+// standard
+#include <cstring>
+
+// local
+#include "ExtractFile.h"
+#include "platform.h"
+#include "util.h"			/* for new_strdup() */
+
+#ifndef PJL_NO_NAMESPACES
+using namespace std;
+#endif
+
+//*****************************************************************************
+//
+// SYNOPSIS
+//
+	/* virtual */ void ExtractFile::parse_value( char *line )
+//
+// DESCRIPTION
+//
+//	Parse the line of text by splitting it into words that are separated by
+//	whitespace.
+//
+// PARAMETERS
+//
+//	line	The line of text to be parsed.
+//
+//*****************************************************************************
+{
+	for ( register char const *s; s = ::strtok( line, " \r\t" ); line = 0 )
+		insert( new_strdup( s ), 0 );
+}
---
+++ swish++/tags/swish++-5.13.5/exit_codes.h
@@ -0,0 +1,75 @@
+/*
+**	SWISH++
+**	exit_codes.h
+**
+**	Copyright (C) 1998  Paul J. Lucas
+**
+**	This program is free software; you can redistribute it and/or modify
+**	it under the terms of the GNU General Public License as published by
+**	the Free Software Foundation; either version 2 of the License, or
+**	(at your option) any later version.
+**
+**	This program is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**	GNU General Public License for more details.
+**
+**	You should have received a copy of the GNU General Public License
+**	along with this program; if not, write to the Free Software
+**	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef	exit_codes_H
+#define	exit_codes_H
+
+// exit(3) status codes
+enum {
+	// common to all executables
+	Exit_Internal_Error		= 127,
+	Exit_Success			= 0,
+	Exit_Config_File		= 1,
+	Exit_Usage			= 2,
+
+	// unique to index
+	Exit_No_Open_Temp		= 10,
+	Exit_No_Write_Index		= 11,
+	Exit_No_Write_Temp		= 12,
+	Exit_Not_Root			= 13,
+
+	// unique to extract
+	Exit_No_Such_File		= 20,
+
+	// common between index and extract
+	Exit_No_Read_Stopwords		= 30,
+
+	// common between index and search
+	Exit_No_Read_Index		= 40,
+
+	// unique to search
+	Exit_Malformed_Query		= 50,
+#ifdef	SEARCH_DAEMON
+	Exit_No_Write_PID		= 51,
+	Exit_No_Host_or_IP		= 52,
+	Exit_No_TCP_Socket		= 53,
+	Exit_No_Unix_Socket		= 54,
+	Exit_No_Unlink			= 55,
+	Exit_No_TCP_Bind		= 56,
+	Exit_No_Unix_Bind		= 57,
+	Exit_No_TCP_Listen		= 58,
+	Exit_No_Unix_Listen		= 59,
+	Exit_No_Select			= 60,
+	Exit_No_Accept			= 61,
+	Exit_No_Fork			= 62,
+	Exit_No_Change_Dir		= 63,
+	Exit_No_Create_Thread		= 64,
+	Exit_No_Detach_Thread		= 65,
+	Exit_No_Init_Thread_Condition	= 66,
+	Exit_No_Init_Thread_Mutex	= 67,
+	Exit_No_User			= 68,
+	Exit_No_Group			= 69,
+#endif
+
+	Exit_End_Enum_Marker
+};
+
+#endif	/* exit_codes_H */
