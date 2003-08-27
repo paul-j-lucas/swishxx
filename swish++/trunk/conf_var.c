095b4a77fbc5e21631120f6ea9ae8b00bdc27669
Paul J. Lucas <paul@lucasmail.org>
2003-08-27 04:23:13 +0000

    Moved call to mmap_file::behavior() to right place.

--- swish++/trunk/conf_var.c
+++ swish++/trunk/conf_var.c
@@ -240,7 +240,6 @@ int conf_var::current_config_file_line_no_ = 0;
 //*****************************************************************************
 {
 	mmap_file const conf_file( file_name );
-	conf_file.behavior( mmap_file::sequential );
 	if ( !conf_file ) {
 		if ( !::strcmp( file_name, ConfigFile_Default ) ) {
 			//
@@ -254,6 +253,7 @@ int conf_var::current_config_file_line_no_ = 0;
 			<< file_name << "\"\n";
 		::exit( Exit_Config_File );
 	}
+	conf_file.behavior( mmap_file::sequential );
 
 	register int line_no = 0;
 	register mmap_file::const_iterator c = conf_file.begin();
