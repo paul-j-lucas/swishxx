4f47a4abcb56ccf9f8bd01d6450ab2823c64392e
Paul J. Lucas <paul@lucasmail.org>
2003-08-28 02:00:32 +0000

    1. s/bcd.c/enc_int.c/ 2. Reworked calc_size() and operator++() for new index file format.

--- swish++/trunk/file_list.c
+++ swish++/trunk/file_list.c
@@ -23,8 +23,9 @@
 #include <cctype>
 
 // local
-#include "bcd.h"
+#include "enc_int.h"
 #include "file_list.h"
+#include "word_markers.h"
 
 //*****************************************************************************
 //
@@ -47,23 +48,36 @@
 	//
 	// It would be nice if there were a way to calculate the size of the
 	// file list other than by just marching though it.  Since this should
-	// be as fast as possible, a much simplified version of the parse_bcd()
-	// code has been inlined here by hand -- twice.  (We also don't care
-	// what the actual numbers are, so there's no point in computing them,
-	// so we save having to do two multiplies, adds, shifts, and logical
-	// ands for each file.)
+	// be as fast as possible, a much simplified version of the dec_int()
+	// code has been inlined here by hand -- a few times.  (We also don't
+	// care what the actual numbers are, so there's no point in computing
+	// them, so we save having to do two shifts, and logical or for each
+	// file.)
 	//
-	for ( register unsigned char const *p = ptr_; *p != 0xFF; ) {
-		while ( (*p++ & 0x0F) != 0x0A ) ;	// skip past file index
-		if ( *p == 0xEE ) {			// META ID list follows
-			while ( *++p != 0xEE ) ;
-			++p;				// skip past 0xEE
-		}
-		while ( (*p++ & 0x0F) != 0x0A ) ;	// skip past occurrences
-		while ( (*p++ & 0x0F) != 0x0A ) ;	// skip past rank
+	register unsigned char const *p = ptr_;
+	while ( true ) {
 		++size_;
+		while ( *p++ & 0x80 ) ;			// skip file index
+		while ( *p++ & 0x80 ) ;			// skip occurrences
+		while ( *p++ & 0x80 ) ;			// skip rank
+
+		bool more_lists = true;
+		while ( more_lists ) {
+			//
+			// At this point, p must be pointing to a marker.
+			//
+			switch ( *p++ ) {
+				case Stop_Marker:
+					return size_;
+				case Word_Entry_Continues_Marker:
+					more_lists = false;
+					break;
+				default:		// skip number
+					while ( *p++ != Stop_Marker ) ;
+			}
+			++p;				// skip marker
+		}
 	}
-	return size_;
 }
 
 //*****************************************************************************
@@ -87,27 +101,47 @@
 //
 //*****************************************************************************
 {
-	if ( *c_ == 0xFF ) {
-		//
-		// Reached the end of the list: set iterator to end.
-		//
-		c_ = 0;
+	if ( !c_ )					// at "end"
 		return *this;
-	}
 
-	v_.index_ = parse_bcd( c_ );
+	v_.index_ = dec_int( c_ );
+	v_.occurrences_ = dec_int( c_ );
+	v_.rank_ = dec_int( c_ );
 
 	if ( !v_.meta_ids_.empty() )
 		v_.meta_ids_.erase( v_.meta_ids_.begin(), v_.meta_ids_.end() );
-	if ( *c_ == 0xEE ) {			// META ID list follows
-		++c_;				// skip past 0xEE
-		while ( *c_ != 0xEE )
-			v_.meta_ids_.insert( parse_bcd( c_ ) );
-		++c_;				// skip past 0xEE
+
+	while ( true ) {
+		//
+		// At this point, c_ must be pointing to a marker.
+		//
+		switch ( *c_++ ) {
+			case Stop_Marker:
+				//
+				// Reached end of file list: set iterator to
+				// end.
+				//
+				c_ = 0;
+				// no break;
+
+			case Word_Entry_Continues_Marker:
+				return *this;
+
+			case Meta_Name_List_Marker:
+				while ( *c_ != Stop_Marker )
+					v_.meta_ids_.insert( dec_int( c_ ) );
+				break;
+
+			default:
+				// Encountered a list marker we don't know
+				// about: we are decoding a possibly future
+				// index file format that has new list types.
+				// Since we don't know what to do with it, just
+				// skip all the numbers in it.
+				//
+				while ( *c_ != Stop_Marker )
+					dec_int( c_ );
+		}
+		++c_;					// skip Stop_Marker
 	}
-
-	v_.occurrences_ = parse_bcd( c_ );
-	v_.rank_ = parse_bcd( c_ );
-
-	return *this;
 }
