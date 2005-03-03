be4a75f5c64fd8d5e0d359d018bdb1e93042723d
Paul J. Lucas <paul@lucasmail.org>
2005-03-03 05:15:56 +0000

    Upped version to 6.0.6.

--- swish++/trunk/version.h
+++ swish++/trunk/version.h
@@ -1,28 +1,28 @@
 /*
-**	SWISH++
-**	version.h
+**      SWISH++
+**      version.h
 **
-**	Copyright (C) 2001  Paul J. Lucas
+**      Copyright (C) 2001  Paul J. Lucas
 **
-**	This program is free software; you can redistribute it and/or modify
-**	it under the terms of the GNU General Public License as published by
-**	the Free Software Foundation; either version 2 of the License, or
-**	(at your option) any later version.
+**      This program is free software; you can redistribute it and/or modify
+**      it under the terms of the GNU General Public License as published by
+**      the Free Software Foundation; either version 2 of the License, or
+**      (at your option) any later version.
 **
-**	This program is distributed in the hope that it will be useful,
-**	but WITHOUT ANY WARRANTY; without even the implied warranty of
-**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**	GNU General Public License for more details.
+**      This program is distributed in the hope that it will be useful,
+**      but WITHOUT ANY WARRANTY; without even the implied warranty of
+**      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**      GNU General Public License for more details.
 **
-**	You should have received a copy of the GNU General Public License
-**	along with this program; if not, write to the Free Software
-**	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**      You should have received a copy of the GNU General Public License
+**      along with this program; if not, write to the Free Software
+**      Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
-#ifndef	version_H
-#define	version_H
+#ifndef version_H
+#define version_H
 
-char const	version[] = "6.0.5";
+char const version[] = "6.0.6";
 
-#endif	/* version_H */
-/* vim:set noet sw=8 ts=8: */
+#endif  /* version_H */
+/* vim:set et sw=4 ts=4: */
