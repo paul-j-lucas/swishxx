a71eeecf307b2197d40475b2f3830c3ace1d0e0f
Paul J. Lucas <paul@lucasmail.org>
2005-07-08 16:32:31 +0000

    Changed tabs to spaces.

--- swish++/trunk/conf_bool.h
+++ swish++/trunk/conf_bool.h
@@ -1,22 +1,22 @@
 /*
-**	SWISH++
-**	conf_bool.h
+**      SWISH++
+**      conf_bool.h
 **
-**	Copyright (C) 1998  Paul J. Lucas
+**      Copyright (C) 1998  Paul J. Lucas
 **
-**	This program is free software; you can redistribute it and/or modify
-**	it under the terms of the GNU General Public License as published by
-**	the Free Software Foundation; either version 2 of the License, or
-**	(at your option) any later version.
+**      This program is free software; you can redistribute it and/or modify
+**      it under the terms of the GNU General Public License as published by
+**      the Free Software Foundation; either version 2 of the License, or
+**      (at your option) any later version.
 **
-**	This program is distributed in the hope that it will be useful,
-**	but WITHOUT ANY WARRANTY; without even the implied warranty of
-**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**	GNU General Public License for more details.
+**      This program is distributed in the hope that it will be useful,
+**      but WITHOUT ANY WARRANTY; without even the implied warranty of
+**      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**      GNU General Public License for more details.
 **
-**	You should have received a copy of the GNU General Public License
-**	along with this program; if not, write to the Free Software
-**	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**      You should have received a copy of the GNU General Public License
+**      along with this program; if not, write to the Free Software
+**      Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
 #ifndef conf_bool_H
@@ -32,46 +32,46 @@
 //
 // SYNOPSIS
 //
-	template<> class conf<bool> : public conf_var
+        template<> class conf<bool> : public conf_var
 //
 // DESCRIPTION
 //
-//	A conf<bool> is-a conf_var for containing the value of a Boolean
-//	configuration variable.
+//      A conf<bool> is-a conf_var for containing the value of a Boolean
+//      configuration variable.
 //
 //*****************************************************************************
 {
 public:
-	operator bool() const { return value_; }
+    operator bool() const { return value_; }
 protected:
-	conf( char const *name, bool default_value );
-	conf<bool>& operator=( bool new_value ) {
-		value_ = new_value;
-		return *this;
-	}
-	CONF_VAR_ASSIGN_OPS( conf<bool> )
+    conf( char const *name, bool default_value );
+    conf<bool>& operator=( bool new_value ) {
+        value_ = new_value;
+        return *this;
+    }
+    CONF_VAR_ASSIGN_OPS( conf<bool> )
 
-	virtual void	parse_value( char *line );
+    virtual void    parse_value( char *line );
 private:
-	bool const	default_value_;
-	bool		value_;
+    bool const      default_value_;
+    bool            value_;
 
-	virtual void	reset() { value_ = default_value_; }
+    virtual void    reset() { value_ = default_value_; }
 };
 
-#define	CONF_BOOL_ASSIGN_OPS(T)			\
-	T& operator=( bool b ) {		\
-		conf<bool>::operator=( b );	\
-		return *this;			\
-	}					\
-	T& operator=( std::string const &s ) {	\
-		conf<bool>::operator=( s );	\
-		return *this;			\
-	}					\
-	T& operator=( char const *s ) {		\
-		conf<bool>::operator=( s );	\
-		return *this;			\
-	}
+#define CONF_BOOL_ASSIGN_OPS(T)             \
+    T& operator=( bool b ) {                \
+        conf<bool>::operator=( b );         \
+        return *this;                       \
+    }                                       \
+    T& operator=( std::string const &s ) {  \
+        conf<bool>::operator=( s );         \
+        return *this;                       \
+    }                                       \
+    T& operator=( char const *s ) {         \
+        conf<bool>::operator=( s );         \
+        return *this;                       \
+    }
 
-#endif	/* conf_bool_H */
-/* vim:set noet sw=8 ts=8: */
+#endif  /* conf_bool_H */
+/* vim:set et sw=4 ts=4: */
--- swish++/trunk/directory.h
+++ swish++/trunk/directory.h
@@ -1,22 +1,22 @@
 /*
-**	SWISH++
-**	directory.h
+**      SWISH++
+**      directory.h
 **
-**	Copyright (C) 1998  Paul J. Lucas
+**      Copyright (C) 1998  Paul J. Lucas
 **
-**	This program is free software; you can redistribute it and/or modify
-**	it under the terms of the GNU General Public License as published by
-**	the Free Software Foundation; either version 2 of the License, or
-**	(at your option) any later version.
+**      This program is free software; you can redistribute it and/or modify
+**      it under the terms of the GNU General Public License as published by
+**      the Free Software Foundation; either version 2 of the License, or
+**      (at your option) any later version.
 **
-**	This program is distributed in the hope that it will be useful,
-**	but WITHOUT ANY WARRANTY; without even the implied warranty of
-**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**	GNU General Public License for more details.
+**      This program is distributed in the hope that it will be useful,
+**      but WITHOUT ANY WARRANTY; without even the implied warranty of
+**      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**      GNU General Public License for more details.
 **
-**	You should have received a copy of the GNU General Public License
-**	along with this program; if not, write to the Free Software
-**	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+**      You should have received a copy of the GNU General Public License
+**      along with this program; if not, write to the Free Software
+**      Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
 #ifndef directory_H
@@ -31,8 +31,8 @@
 typedef std::map< char const*, int > dir_set_type;
 extern dir_set_type dir_set;
 //
-//	This contains a map of all directory paths and an int that gives its
-//	index (the directory number in the order encountered).
+//      This contains a map of all directory paths and an int that gives its
+//      index (the directory number in the order encountered).
 
-#endif	/* directory_H */
-/* vim:set noet sw=8 ts=8: */
+#endif  /* directory_H */
+/* vim:set et sw=4 ts=4: */
